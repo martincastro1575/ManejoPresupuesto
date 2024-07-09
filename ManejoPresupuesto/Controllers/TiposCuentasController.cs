e4e7ef199e104563f189e6012537bdcf2269c1a8
Martin Castro <martin.castro1575@gmail.com>
2024-07-09 13:39:34 -0300

    First commit"

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+
+    {
+        private readonly string connectionString;
+        public TiposCuentasController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public IActionResult Crear() 
+        { 
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = connection.Query("Select 1").FirstOrDefault();
+            }
+            return View();
+        
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipocuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipocuenta);
+            }
+
+            return View();
+        }
+    }
+
+
+}
