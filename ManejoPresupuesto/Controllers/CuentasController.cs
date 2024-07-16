952a1e8da17e16eabcd819d579e2d5b6c41084ee
Martin Castro <martin.castro1575@gmail.com>
2024-07-16 20:49:08 -0300

    Creacion de Entidad Cuentas y vistas

---
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,31 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class CuentasController:Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServiciosUsuarios serviciosUsuarios;
+
+        public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServiciosUsuarios serviciosUsuarios)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.serviciosUsuarios = serviciosUsuarios;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Crear()
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            var modelo = new CuentaCreacionViewModel();
+
+            modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+
+            return View(modelo);
+        }
+    }
+}
