01aadd767b085a78fcbea0b56e17d0005ba685cc
Martin Castro <martin.castro1575@gmail.com>
2024-07-22 14:07:36 -0300

    Creando Cuentas

--- ManejoPresupuesto/Controllers/CuentasController.cs
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -2,6 +2,7 @@
 using ManejoPresupuesto.Servicios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Reflection;
 
 namespace ManejoPresupuesto.Controllers
 {
@@ -9,23 +10,71 @@ namespace ManejoPresupuesto.Controllers
     {
         private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
         private readonly IServiciosUsuarios serviciosUsuarios;
+        private readonly IRepositorioCuentas repositorioCuentas;
 
-        public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServiciosUsuarios serviciosUsuarios)
+        public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServiciosUsuarios serviciosUsuarios, IRepositorioCuentas repositorioCuentas)
         {
             this.repositorioTiposCuentas = repositorioTiposCuentas;
             this.serviciosUsuarios = serviciosUsuarios;
+            this.repositorioCuentas = repositorioCuentas;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var cuentasConTipoCuenta = await repositorioCuentas.Buscar(usuarioId);
+
+            var modelo = cuentasConTipoCuenta
+                .GroupBy(x => x.TipoCuenta)
+                .Select(grupo => new IndiceCuentasViewModel
+                {
+                    TipoCuenta = grupo.Key,
+                    Cuentas = grupo.AsEnumerable()
+                }).ToList();
+            return View(modelo);
         }
 
         [HttpGet]
         public async Task<IActionResult> Crear()
         {
             var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
-            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            
             var modelo = new CuentaCreacionViewModel();
 
-            modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+            modelo.TiposCuentas = await ObtenerTiposCuentas(usuarioId);
 
             return View(modelo);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(CuentaCreacionViewModel cuenta)
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(cuenta.TipoCuentaId, usuarioId);
+
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                cuenta.TiposCuentas = await ObtenerTiposCuentas(usuarioId);
+
+                return View(cuenta);
+            }
+
+            await repositorioCuentas.Crear(cuenta);
+
+            return RedirectToAction("Index");
+        }
+
+        private async Task<IEnumerable<SelectListItem>> ObtenerTiposCuentas(int usuarioId)
+        {
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            return tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
         }
     }
 }
---
+++ ManejoPresupuesto/Servicios/RepositorioCuentas.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioCuentas
+    {
+        Task<IEnumerable<Cuenta>> Buscar(int UsuarioId);
+        Task Crear(Cuenta cuenta);
+    }
+    public class RepositorioCuentas: IRepositorioCuentas
+    {
+        private readonly string connectionString;
+        public RepositorioCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            
+        }
+
+        public async Task Crear(Cuenta cuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var id = await connection.QuerySingleAsync<int>(
+                            @"INSERT INTO Cuentas (Nombre,TipoCuentaId,Balance,Descripcion) 
+                            VALUES (@Nombre,@TipoCuentaId,@Balance,@Descripcion);
+                            
+                            SELECT SCOPE_IDENTITY();", cuenta);
+
+            cuenta.Id = id;
+        }
+
+        public async Task<IEnumerable<Cuenta>> Buscar(int UsuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            return await connection.QueryAsync<Cuenta>(
+                @"SELECT c.id, c.Nombre, c.Balance,tc.Nombre as TipoCuenta 
+                FROM Cuentas c INNER JOIN TiposCuentas tc ON tc.id = c.TipoCuentaId 
+                WHERE tc.UsuarioId = @UsuarioId ORDER BY tc.Orden;", new { UsuarioId });
+        }
+
+
+    }
+}
