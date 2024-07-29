b903ac2388564b310b2287480953a0dfd1a48e5e
Martin Castro <martin.castro1575@gmail.com>
2024-07-29 19:26:39 -0300

    Borrando categorias

--- ManejoPresupuesto/Controllers/CategoriasController.cs
+++ ManejoPresupuesto/Controllers/CategoriasController.cs
@@ -87,5 +87,44 @@ namespace ManejoPresupuesto.Controllers
 
 
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Borrar(int id)
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(id, usuarioId);
+
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BorrarCategoria(int id)
+        {            
+
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(id, usuarioId);
+
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            
+            await repositorioCategorias.Borrar(id);
+
+            return RedirectToAction("Index");
+
+
+        }
+
+
+
+
+
     }
 }
--- ManejoPresupuesto/Servicios/RepositorioCategorias.cs
+++ ManejoPresupuesto/Servicios/RepositorioCategorias.cs
@@ -8,6 +8,7 @@ namespace ManejoPresupuesto.Servicios
     public interface IRepositorioCategorias
     {
         Task Actualizar(Categoria categoria);
+        Task Borrar(int id);
         Task Crear(Categoria categoria);
         Task<IEnumerable<Categoria>> Obtener(int usuarioId);
         Task<Categoria> ObtenerPorId(int id, int usuarioId);
@@ -54,6 +55,13 @@ namespace ManejoPresupuesto.Servicios
             using var connection = new SqlConnection(connectionString);
 
             await connection.ExecuteAsync(@"UPDATE Categorias SET Nombre = @Nombre, TipoOperacionId = @TipoOperacionId WHERE Id = @Id", categoria);
+        }
+
+        public async Task Borrar(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            await connection.ExecuteAsync("DELETE Categorias WHERE id = @id", new { id });
         }
 
 
